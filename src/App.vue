fcc2b7d0050514cdba8e7e22b44943679cd038ce
bugsagweler <bugsagweler@gmail.com>
2025-12-11 12:35:24 +0200

    Checkout name changed

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
+  <div id="app">
     <AppHeader />
-    <div id="app" class="main-content">
+    <div class="main-content">
       <CourseList />
-      <Checkout />
+      <CheckoutSidebar />
     </div>
   </div>
 </template>
@@ -11,38 +11,16 @@
 <script setup>
 import AppHeader from './components/AppHeader.vue'
 import CourseList from './components/CourseList.vue'
-import Checkout from './components/Checkout.vue'
+import CheckoutSidebar from './components/CheckoutSidebar.vue'
 </script>
 
 <style>
-body {
-  font-family: sans-serif;
-  background-image: url('/images/background.jpg');
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  margin: 0;
-  padding: 0;
-}
-
-#app {
-  max-width: 1400px;
-  margin: 0 auto;
-  padding: 1rem;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-}
-
-/* Main content: course list + checkout */
 .main-content {
   display: flex;
   gap: 2rem;
-  flex-wrap: wrap;
 }
 
-/* Stack on small screens */
+/* Make responsive: stack on small screens */
 @media (max-width: 768px) {
   .main-content {
     flex-direction: column;
